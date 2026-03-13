98432397ffa00f92fbe1471809553332a303c128
carlos sjs <carlos.sjs@outlook.com>
2026-03-13 01:30:39 -0600

    Feat: Create API endpoint to get a list of playlists (pending support for spotify)

---
+++ pullrequest-backend/src/routes/playlists.ts
@@ -0,0 +1,47 @@
+import { Elysia, t } from 'elysia';
+import { youtube_v3 } from 'googleapis';
+import { createYouTubeClient } from '@services/youtube_api_client';
+
+export const playlistsRoutes = new Elysia({ prefix: 'playlists' })
+  .get('/list', async ({ query, headers, status }) => {
+    const { platform } = query;
+
+    const deviceId = headers['x-device-id'];
+    if (!deviceId) {
+      return;
+    }
+
+    if (platform === 'ytm') {
+      const youtube = createYouTubeClient(deviceId);
+      if (!youtube) return status(401, 'User not authenticated with Google');
+
+      const playlists: youtube_v3.Schema$Playlist[] = [];
+      let paginationToken: string | undefined = undefined;
+
+      while (true) {
+        const response: any = await youtube.playlists.list({
+          part: ['snippet', 'contentDetails'],
+          mine: true,
+          maxResults: 50,
+          pageToken: paginationToken
+        });
+
+        if (response.data.items) {
+          playlists.push(...response.data.items);
+        }
+
+        paginationToken = response.data.nextPageToken ?? undefined;
+        if (!paginationToken) break;
+      }
+
+      return playlists;
+    }
+
+  }, {
+    query: t.Object({
+      platform: t.Union([
+        t.Literal('ytm'),
+        t.Literal('spotify')
+      ])
+    })
+  });

cd5a14fd12fbf07af81df50a9e03eee472d5fc0c
carlos sjs <carlos.sjs@outlook.com>
2026-03-23 20:04:32 -0600

    Feat: Add API routes to index.ts

--- pullrequest-backend/src/index.ts
+++ pullrequest-backend/src/index.ts
@@ -1,7 +1,17 @@
 import { Elysia } from "elysia";
+import { deviceAuthPlugin } from "@services/device_auth";
+import { registerRoute } from "@routes/register";
+import { oauthRoute } from "@routes/oauth";
+import { playlistsRoutes } from "@routes/playlists";
+import { playlistTransferRoutes } from "@routes/transfer";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get('/', () => 'Hi 🫣')
+  .use(registerRoute)
+  .use(deviceAuthPlugin)
+  .use(oauthRoute)
+  .use(playlistTransferRoutes)
+  .use(playlistsRoutes)
+  .listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log('🦊 Server is running')
